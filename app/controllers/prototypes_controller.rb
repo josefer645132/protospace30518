677b83cb80b34acdc50f79188e4dfa8e4da25d8e
josefer645132 <yajima.shintaro@gmail.com>
2020-10-22 16:27:37 +0900

    修正５

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,30 +1,13 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-def new
-  @prototype = Prototype.new
-end
-
-
-def index
-  @prototypes = Prototype.all
-end
-
-
-def show
-  @prototype = Prototype.find(params[:id])
-  @comment = Comment.new
-  @comments = @prototype.comments
- 
-end
-
-
-def edit
-  @prototype = Prototype.find(params[:id])
-  unless current_user == @prototype.user
-    redirect_to root_path
+  def index
+    @prototypes = Prototype.all
   end
-end
+
+  def new
+    @prototype = Prototype.new
+  end
 
   def create
     @prototype = Prototype.new(prototype_params)
@@ -35,8 +18,21 @@ end
     end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+
   def update
-  @prototype = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -44,17 +40,15 @@ end
     end
   end
 
- def prototype_params
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-  
-def destroy
-  prototype = Prototype.find(params[:id])
-  prototype.destroy
-  redirect_to root_path
-end
-
-
-
- 
 end
